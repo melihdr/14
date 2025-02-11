d8b14cbd472723fb85c4b5530b43cf91b0e4103f
melihdr <meljhdurmus@gmail.com>
2025-02-11 23:19:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let after_part = document.querySelector(".after-part");
+let letter_image = document.querySelector(".letter-image");
+let message = document.querySelector(".message");
+
+letter_button = document
+  .querySelector(".letter-button")
+  .addEventListener("click", () => {
+    console.log("11");
+    after_part.innerHTML =
+      "          <img style='width:60vw' class='opened_letter' src='opened_letter.jpg' alt='opened letter' />";
+
+    letter_image.innerHTML = "";
+    message.innerHTML = "";
+  });
