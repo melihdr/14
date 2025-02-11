335244b8ea6dc92ce67eceefd6da7056356d31e6
melihdr <meljhdurmus@gmail.com>
2025-02-12 00:51:03 +0300

    ended

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let letter_button = document.querySelector(".letter-button");
 let first = document.querySelector(".first");
+let hiddenImages = document.querySelectorAll(".hidden");
 
 letter_button.addEventListener("click", () => {
   first.innerHTML =
     "<img style='width:100%; height:auto; margin-left: 15px; margin-right:15px;' src='opened_letter.png'/>";
+  hiddenImages.forEach((image) => {
+    image.style.visibility = "visible";
+  });
 });
