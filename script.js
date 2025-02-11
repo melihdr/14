e798a19cd46980188fd38b70a4a7122d233d55c5
melihdr <meljhdurmus@gmail.com>
2025-02-12 00:33:00 +0300

    last

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-let after_part = document.querySelector(".after-part");
-let letter_image = document.querySelector(".letter-image");
-let message = document.querySelector(".message");
+let letter_button = document.querySelector(".letter-button");
+let first = document.querySelector(".first");
 
-letter_button = document
-  .querySelector(".letter-button")
-  .addEventListener("click", () => {
-    console.log("11");
-    after_part.innerHTML =
-      "          <img style='width:60vw; min-width: 500px; max-width:500px;' class='opened_letter' src='opened_letter.jpg' alt='opened letter' />";
-
-    letter_image.innerHTML = "";
-    message.innerHTML = "";
-  });
+letter_button.addEventListener("click", () => {
+  first.innerHTML =
+    "<img style='width:100%; height:auto; margin-left: 15px; margin-right:15px;' src='opened_letter.png'/>";
+});
